76bcfc60a9a9212109ce42599c25a95baab92c45
sailendraprasath <sailendraprasath05@gmail.com>
2024-10-27 20:24:10 +0530

    i have done 16

---
+++ Oct27/formonth.py
@@ -0,0 +1,6 @@
+for month in range(1,13):
+    print(f"Month:- {month}")
+    for week in range(1,5):
+        print(f"\tweek:- {week}")
+        for days in range(1,8):
+            print(f"\t\tDays:- {days}")
