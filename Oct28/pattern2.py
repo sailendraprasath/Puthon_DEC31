7890a3fbdb2567002fc35f4f76fce6728533b4b3
sailendraprasath <sailendraprasath05@gmail.com>
2024-10-28 22:37:09 +0530

    i have done day 17

---
+++ Oct28/pattern2.py
@@ -0,0 +1,7 @@
+"""using nested for loop ahm paa"""
+
+
+for i in range(1,6):
+    for j in range(i):
+        print("*",end=" ")
+    print()
---
+++ Oct28/pattern3.py
@@ -0,0 +1,7 @@
+"""pattern in number using nested for loop"""
+
+
+for i in range(1,6):
+    for j in range(i):
+        print(i,end=" ")
+    print()
