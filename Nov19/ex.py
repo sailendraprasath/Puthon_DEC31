318ed59f1482c38691b05a23f9809c09471d1bbe
sailendraprasath <sailendraprasath05@gmail.com>
2024-11-19 23:24:22 +0530

    i have done 39 days

---
+++ Nov19/ex.py
@@ -0,0 +1,6 @@
+"""print 10,20,30,....200 using while loop"""
+i = 10
+
+while(i<=200):
+    print(i,end=" ")
+    i += 10
---
+++ Nov19/ex2.py
@@ -0,0 +1,6 @@
+"""Reversed natural numbers"""
+i = 10
+
+while(i > 0):
+    print(i,end=" ")
+    i-=1
