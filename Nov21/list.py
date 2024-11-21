bd415ad652cbfc59c1097a782ebffdb4a17cb482
sailendraprasath <sailendraprasath05@gmail.com>
2024-11-21 22:30:10 +0530

    i have done 41 days

---
+++ Nov21/list.py
@@ -0,0 +1,9 @@
+a = [1,2,3,4,5,6]
+print(type(a))
+
+a.append(7)  #append use panna last la insert aagum
+a.append('Sailesh')
+a.append(True)
+a.insert(0,'sailesh') #insert use panna list la starting la insert aagum
+a[2] = 'vanakam' #namaku theva padura idathula insert pantu alredy iruntha value va thookirlam
+print(a)
