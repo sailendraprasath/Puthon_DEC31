5b724724d98b9c21b1f5eb4a7c4b2530905231c9
sailendraprasath <sailendraprasath05@gmail.com>
2024-12-07 00:58:05 +0530

    i have done

---
+++ Dec06/Dic.py
@@ -0,0 +1,20 @@
+a= {
+    "name":"Sailesh",
+    "age":19,
+    "Location":"Madurai",
+    "LPA":"17 LPA"
+}
+
+print(a)
+# print(a["name"]) ipde namma key  kudutha value varum
+print(a["name"]) 
+
+print(a.keys())
+print(a.values())
+
+
+print("_________________________")
+
+a.update({"age":20})
+
+print(a)
---
+++ Dec06/Dic2.py
@@ -0,0 +1,8 @@
+Personal_Detail = {
+    "name":"Sailendra prasath",
+    "age":20,
+    "Education":"B.tech,CSBS",
+    "Salary":"17LPA"
+}
+
+print(Personal_Detail)
