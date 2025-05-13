0015a88afd4214bd3a4ca290be566d08eed9d2e5
sailendraprasath <sailendraprasath05@gmail.com>
2025-05-14 01:17:13 +0530

    well

--- May08/demo7.py
+++ May08/demo7.py
@@ -1,8 +1,14 @@
-a = 20
-b = 10
-a = a - b
-b = b + a
-print(a)
-print(b) 
+num1 = int(input("Enter num1 value: "))
+num2 = int(input("Enter num2 value: "))
+num3 = int(input("Enter num3 value: "))
 
+if num1 < num2:
+    if num1 < num3:
+        print("num1 is the smallest")
+    else:
+        print("num3 is the smallest")
+elif num2 < num3:
+    print("num2 is the smallest")
+else:
+    print("num3 is the smallest")       
 
