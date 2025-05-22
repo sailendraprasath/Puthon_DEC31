5641859c6dec0653d5daf605af0711cd8c3ec9e3
sailendraprasath <sailendraprasath05@gmail.com>
2025-05-22 21:32:36 +0530

    well

--- May21/demo3.py
+++ May21/demo3.py
@@ -1,3 +1,18 @@
+# num = 153
+
+# count = 0
+# store = 0
+
+# while num > 0:
+#     store = num % 10
+#     for i in range(1):
+#         count += store ** 3
+#     num = num // 10
+
+# if count == num:
+#     print("Armstrong")
+# else:
+#     print("Not Armstrong")
 num = 153
 
 count = 0
