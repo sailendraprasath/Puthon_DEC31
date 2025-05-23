44bc4e7440bbfb3cd7bc8a65483c6c8dcd317034
sailendraprasath <sailendraprasath05@gmail.com>
2025-05-23 20:40:04 +0530

    well

---
+++ May23/demo4.py
@@ -0,0 +1,12 @@
+def star():
+    print("* * * * * * * *")
+def display():
+    star()
+    print("     Hello World")
+    star()
+    print()
+    star()
+    print("     Welcome")
+    star()
+
+display()
