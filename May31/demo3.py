06b0681a27c849540d7e3c7e954afa69dcb75a62
sailendraprasath <sailendraprasath05@gmail.com>
2025-06-01 21:10:09 +0530

    well

--- May31/demo3.py
+++ May31/demo3.py
@@ -1,3 +1,13 @@
+# num = int(input("Enter a number: "))
+
+# a=2
+# while a < num:
+#     if num % a == 0:
+#         print("Not a prime")
+#         break
+#     a += 1
+# else:
+#     print("prime")
 num = int(input("Enter a number: "))
 
 a=2
