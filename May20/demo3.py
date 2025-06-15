cf60988ad3b1da8fa085a421bf54d6561afe9d79
sailendraprasath <sailendraprasath05@gmail.com>
2025-06-15 18:57:46 +0530

    well

--- May20/demo3.py
+++ May20/demo3.py
@@ -1,9 +1,3 @@
-# num = int(input("Enter a number: "))
-# count = 0
-# while num > 0:
-#     num //= 10
-#     count += 1
-# print("Number of digits:", count)
 num = int(input("Enter a number: "))
 count = 0
 while num > 0:
