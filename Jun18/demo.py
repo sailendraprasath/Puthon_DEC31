5204e583c7b2b81be68dd126acedf72480b646b6
sailendraprasath <sailendraprasath05@gmail.com>
2025-06-18 13:29:12 +0530

    well

---
+++ Jun18/demo.py
@@ -0,0 +1,7 @@
+val1 = ("Sailesh",21,"CSBS","Madurai")
+val2 = ["Sailesh",21,"CSBS","Madurai"]
+# print(type(val))
+val1[0] = "Sailendra prasath"
+val2[0] = "Sailendra prasath"
+print(val1[0])
+print(val2[0])
