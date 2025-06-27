3919636086c8c559f1937d17402aae3b48346ca2
sailendraprasath <sailendraprasath05@gmail.com>
2025-06-28 03:12:22 +0530

    well

--- Jun27/demo.py
+++ Jun27/demo.py
@@ -1,10 +1,14 @@
-num = [2,4,5,5]
-target = 10
-
-for i in range(len(num)):
-    for j in range(i+1,len(num)):
-        if num[i] + num[j] == target:
-            print("Found ",num[i], "and", num[j],"equals to",target)
-            break
-    else:
-        continue
+def addTwo(num1,target1):
+    for i in range(len(num1)):
+        for j in range(i+1,len(num1)):
+            if num1[i] + num1[j] == target1:
+                return num1[i]+num1[j]
+get_num = int(input("Enter a number: "))
+num = []
+for i in range(get_num):
+    number = int(input("Enter a Digit: "))
+    num.append(number)
+get_target = int(input("Enter your target: "))
+target = get_target
+val = addTwo(num,target)
+print(val,"is catch a targeted value ",target)
