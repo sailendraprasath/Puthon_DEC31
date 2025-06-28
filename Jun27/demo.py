a29f91eb651f34026d5be5a3c11ee02bed719fe1
sailendraprasath <sailendraprasath05@gmail.com>
2025-06-28 23:03:26 +0530

    well

--- Jun27/demo.py
+++ Jun27/demo.py
@@ -1,14 +1,10 @@
-def addTwo(num1,target1):
-    for i in range(len(num1)):
-        for j in range(i+1,len(num1)):
-            if num1[i] + num1[j] == target1:
-                return num1[i]+num1[j]
-get_num = int(input("Enter a number: "))
-num = []
-for i in range(get_num):
-    number = int(input("Enter a Digit: "))
-    num.append(number)
-get_target = int(input("Enter your target: "))
-target = get_target
-val = addTwo(num,target)
-print(val,"is catch a targeted value ",target)
+a = 0
+b = 1
+num = int(input("Enter a number: "))
+#  Example for n = 10: 
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34    
+for i in range(num):
+    print(a)
+    c = a + b
+    a = b
+    b = c
