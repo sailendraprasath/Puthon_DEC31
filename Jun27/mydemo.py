03ce777e410b35600500eb784ff3449260c2401e
sailendraprasath <sailendraprasath05@gmail.com>
2025-07-02 23:20:49 +0530

    well

--- Jun27/mydemo.py
+++ Jun27/mydemo.py
@@ -1,14 +1,3 @@
-# num = 28
-# a=1
-# store = 0
-# while a < num:
-#     if num % a == 0:
-#         store+=a
-#     a += 1
-# if store == num:
-#     print("Perfect number")
-# else:
-#     print("Not a perfect number")
 num = 28
 a=1
 store = 0
