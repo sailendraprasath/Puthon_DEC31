d7b3165debc0e74a93d4510d619f1b5af17b21ad
sailendraprasath <sailendraprasath05@gmail.com>
2025-07-13 22:05:10 +0530

    well

--- Jul11/demo.py
+++ Jul11/demo.py
@@ -1,15 +1,3 @@
-# words = "a1b2c3"
-# letters = []
-# num = []
-# for i in range(len(words)):
-#     if i % 2 != 0:
-#         num.append(words[i])       
-#     else:
-#         letters.append(words[i])   
-# else:
-#     for i in range(len(num)):
-#         for j in range(int(num[i])):
-#             print(letters[i],end="")
 words = "a1b2c3"
 letters = []
 num = []
