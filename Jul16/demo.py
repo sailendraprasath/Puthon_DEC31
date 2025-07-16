0517a090483c049a5d2056c257359a39490a197e
sailendraprasath <sailendraprasath05@gmail.com>
2025-07-16 22:03:08 +0530

    well

--- Jul16/demo.py
+++ Jul16/demo.py
@@ -1,24 +1,9 @@
-s1 = "listen"
-s2 = "silent"
-
-if len(s1) != len(s2):
-    print("Not a anagram")
-else:
-    num1 = {}
-    num2 = {}
-
-    for num in s1:
-        if num  in num1:
-            num1[num] += 1
-        else:
-            num1[num] = 1
-
-    for num in s2:
-        if num  in num2:
-            num2[num] += 1
-        else:
-            num2[num] = 1
-    if num1 == num2:
-        print("Anagram")
+words = "aaabbc"
+store = {}
+for i in words:
+    if i in store:
+        store[i] += 1
     else:
-        print("Not a Anagram")
+        store[i] = 1
+for letter,count in store.items():
+    print("letters",letter,"count",count)
