1b83ac00eb8bb3264b87d4ff6c491befdbf96050
sailendraprasath <sailendraprasath05@gmail.com>
2025-07-16 21:59:25 +0530

    well

--- Jul16/demo.py
+++ Jul16/demo.py
@@ -1,4 +1,24 @@
-words = input("Enter a words: ")
-val = words.split()
-val = "-".join(val)
-print(val)
+s1 = "listen"
+s2 = "silent"
+
+if len(s1) != len(s2):
+    print("Not a anagram")
+else:
+    num1 = {}
+    num2 = {}
+
+    for num in s1:
+        if num  in num1:
+            num1[num] += 1
+        else:
+            num1[num] = 1
+
+    for num in s2:
+        if num  in num2:
+            num2[num] += 1
+        else:
+            num2[num] = 1
+    if num1 == num2:
+        print("Anagram")
+    else:
+        print("Not a Anagram")
