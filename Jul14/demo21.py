c50162c4ec9122d2a2b4366b34d87b5bb2e226ec
sailendraprasath <sailendraprasath05@gmail.com>
2025-07-16 21:30:23 +0530

    well

--- Jul14/demo21.py
+++ Jul14/demo21.py
@@ -1,6 +1,16 @@
-words = "a1b2c3d2"
+# words = "a1b2c3d2"
 
-for i in range(0, len(words),2):
-    letter = words[i]
-    count = int(words[i + 1])
-    print(letter*count,end="")
+# for i in range(0, len(words),2):
+#     letter = words[i]
+#     count = int(words[i + 1])
+#     print(letter*count,end="")
+
+words = input()
+b = {}
+for i in words:
+    if i in b:
+        b[i] += 1
+    else:
+        b[i] = 1
+for i,j in b.items():
+    print("words",i,"Count",j)
