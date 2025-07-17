d2c25e450484865281a688f68bd893fe0ed64f9c
sailendraprasath <sailendraprasath05@gmail.com>
2025-07-17 22:10:07 +0530

    well

---
+++ Jul17/demo.py
@@ -0,0 +1,16 @@
+def long_subset(arr):
+    result = []
+    unique_val = set(arr)
+
+    for num in unique_val:
+        subset = []
+
+        for x in arr:
+            if x == num or x == num+2:
+                subset.append(x)
+        if len(subset) > len(result):
+            result = subset
+    return result 
+
+arr = [2, 2, 4, 4, 5, 5, 7, 5, 7]
+print(long_subset(arr))
