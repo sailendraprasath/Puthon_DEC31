a59764363c09979782b87bff0f875398181f11f4
sailendraprasath <sailendraprasath05@gmail.com>
2025-07-18 16:53:09 +0530

    well

--- Jul17/demo1.py
+++ Jul17/demo1.py
@@ -1,12 +1,29 @@
-words = "Majulah Info Tech Systems"
+words = input("Enter a words: ")
 split_words = words.split()
-result = ""
+current_list = split_words.copy()
+output_lines = []
+# "Majulah Info Tech Systems"
+while any(current_list): 
+    collected = ""
+    new_list = []
 
-while True:
-    if words == None:
-        break
-    else:
-        for i in split_words:
-            result += i[:2]
+    for index, word in enumerate(current_list):
+        if not word:
+            new_list.append("")
+            continue
+        if index % 2 == 0:
+            part = word[:2]
+            new_word = word[2:]
         else:
-            result += i[-2:]
+            part = word[-2:]
+            new_word = word[:-2]
+
+        collected += part
+        new_list.append(new_word)
+
+    output_lines.append(collected)
+    current_list = new_list
+
+print(output_lines)
+for line in output_lines:
+    print(line)
