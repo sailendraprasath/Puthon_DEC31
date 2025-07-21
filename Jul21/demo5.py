0fc49fd259e991ee2e8ed57a0a3214567d26b877
sailendraprasath <sailendraprasath05@gmail.com>
2025-07-21 20:48:50 +0530

    DigiSME

--- Jul21/demo5.py
+++ Jul21/demo5.py
@@ -1,19 +1,13 @@
 def longest_common_suffix(strs):
     if not strs:
         return ""
-
-
     reversed_strs = [s[::-1] for s in strs]
-
-
     prefix = reversed_strs[0]
     for word in reversed_strs[1:]:
         while not word.startswith(prefix):
             prefix = prefix[:-1]
             if not prefix:
                 return ""
-
-
     return prefix[::-1]
 
 strs = input("Enter words separated by space: ").split()
