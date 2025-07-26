014fa4be505656d10ac03a62fba7bea4ca3c7d6f
sailendraprasath <sailendraprasath05@gmail.com>
2025-07-26 22:30:54 +0530

    well

--- Jul21/demo5.py
+++ Jul21/demo5.py
@@ -1,3 +1,17 @@
+# def longest_common_suffix(strs):
+#     if not strs:
+#         return ""
+#     reversed_strs = [s[::-1] for s in strs]
+#     prefix = reversed_strs[0]
+#     for word in reversed_strs[1:]:
+#         while not word.startswith(prefix):
+#             prefix = prefix[:-1]
+#             if not prefix:
+#                 return ""
+#     return prefix[::-1]
+
+# strs = input("Enter words separated by space: ").split()
+# print("Longest Common Suffix:", longest_common_suffix(strs))
 def longest_common_suffix(strs):
     if not strs:
         return ""
