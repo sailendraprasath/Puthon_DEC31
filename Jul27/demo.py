be9c09ca150c152d1f36c3bcb1308a02003a2199
sailendraprasath <sailendraprasath05@gmail.com>
2025-08-03 22:47:19 +0530

    well

--- Jul27/demo.py
+++ Jul27/demo.py
@@ -1,3 +1,8 @@
+# for i in range(2):
+#     print(" ",end=" ")
+#     for j in range(2):
+#         print("*",end=" ")
+#     print()
 for i in range(2):
     print(" ",end=" ")
     for j in range(2):
