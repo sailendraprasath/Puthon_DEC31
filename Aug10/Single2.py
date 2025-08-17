94ef70099a66ae22edc2727914ad51a2ecb53571
sailendraprasath <sailendraprasath05@gmail.com>
2025-08-17 23:16:36 +0530

    well

--- Aug10/Single2.py
+++ Aug10/Single2.py
@@ -1,17 +1,3 @@
-# class Teacher:
-#     def __init__(self,name):
-#         self.name = name
-
-# class Student(Teacher):
-
-#     def __init__(self, name):
-#         super().__init__(name)
-
-#     def display(self):
-#         print("Student Teacher Name is:",self.name)
-
-# obj = Student("Sailesh")
-# obj.display()
 class Teacher:
     def __init__(self,name):
         self.name = name
@@ -26,3 +12,4 @@ class Student(Teacher):
 
 obj = Student("Sailesh")
 obj.display()
+
