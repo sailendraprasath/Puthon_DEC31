5896dfb2a707326075a5a98925c8d57061a563b6
sailendraprasath <sailendraprasath05@gmail.com>
2025-10-04 23:14:11 +0530

    well

--- Aug10/Db.py
+++ Aug10/Db.py
@@ -1,11 +1,11 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='root',
-    database='avasoft'
-)
+# mydb = mysql.connector.connect(
+#     host='localhost',
+#     user='root',
+#     password='root',
+#     database='avasoft'
+# )
 
 print("Connection sucsessfully")
 
