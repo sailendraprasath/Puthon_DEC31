668f1aa0458819d84585cb1f6139115748acc770
sailendraprasath <sailendraprasath05@gmail.com>
2025-10-06 23:59:35 +0530

    well

--- Aug10/Db.py
+++ Aug10/Db.py
@@ -1,20 +1,21 @@
 import mysql.connector
 
-# mydb = mysql.connector.connect(
-#     host='localhost',
-#     user='root',
-#     password='root',
-#     database='avasoft'
-# )
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='root',
+    database='avasoft'
+)
 
 print("Connection sucsessfully")
 
 my_Cursor = mydb.cursor()
-# my_Cursor.execute("CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), age INT)")
-# print('Table Created Sucessfully')
+# summa
+my_Cursor.execute("CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), age INT)")
+print('Table Created Sucessfully')
 
-# sql = "INSERT INTO students (name, age) VALUES (%s, %s)"
-# val = ("Praveen", 21)
+sql = "INSERT INTO students (name, age) VALUES (%s, %s)"
+val = ("Praveen", 21)
 
 # my_Cursor.execute(sql, val)
 # mydb.commit()   # save changes
