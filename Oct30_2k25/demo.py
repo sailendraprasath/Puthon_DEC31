c098dd83b99bf8c5da95fe7737cc0b9813a66b15
sailendraprasath <sailendraprasath05@gmail.com>
2025-10-30 23:36:31 +0530

    well

---
+++ Oct30_2k25/demo.py
@@ -0,0 +1,13 @@
+class Demo:
+    def add(self,*args):
+        digit = 0
+        for val in args:
+            digit += val
+        return digit
+
+obj = Demo()
+sailesh = obj.add(10,50,36,15)
+hema = obj.add(25,60,77)
+vaishu = obj.add(33,45,12,75)
+
+print(sailesh);print(hema);print(vaishu)
