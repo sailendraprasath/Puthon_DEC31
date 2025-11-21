ee68a18711150e540e5b4ffd263b6f2337421b42
sailendraprasath <sailendraprasath05@gmail.com>
2025-11-21 09:32:06 +0530

    well

--- Nov14_2K25/demo.py
+++ Nov14_2K25/demo.py
@@ -1,17 +1,40 @@
-import mysql.connector
+class LapTop:
+    def __init__(self,name,amt):
+        self.name = name
+        self.amt = amt
+    def display(self):
+        print(f"Laptop name: {self.name} and Amount: {self.amt}")
 
-con = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database ="College"
-)
-print("Database connected successfully")
-cursor = con.cursor()
-sql ="insert into student (id,name,age) values (%s,%s,%s)"
-value = (1,"Sailesh",21)
-cursor.execute(sql,value)
-con.commit()
-print(cursor.rowcount,"record inserted")
-cursor.close()
-con.close()
+lap1 = LapTop("Victus",75000)
+lap1.display()
+
+
+
+lap2 = LapTop("Asus",85000)
+lap2.display()
+
+lap3 = LapTop("MacBook",125000)
+lap3.display()
+
+lap4 = LapTop("Dell",95000)
+lap4.display()
+
+
+lap5 = LapTop("Acer",65000)
+lap5.display()
+
+
+lap6 = LapTop("Lenovo",70000)
+lap6.display()
+lap7 = LapTop("HP",80000)
+lap7.display()
+
+lap8 = LapTop("MSI",120000)
+lap8.display()
+
+
+lap9 = LapTop("Razer",150000)
+lap9.display()
+
+lap10 = LapTop("Samsung",90000)
+lap10.display()
