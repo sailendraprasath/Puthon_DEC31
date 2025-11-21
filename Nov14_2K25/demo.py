255dffe04576273937f33136119c5171233b75dc
sailendraprasath <sailendraprasath05@gmail.com>
2025-11-21 09:40:34 +0530

    well

--- Nov14_2K25/demo.py
+++ Nov14_2K25/demo.py
@@ -1,40 +1,11 @@
-class LapTop:
-    def __init__(self,name,amt):
+class Person:
+    def __init__(self,name):
         self.name = name
-        self.amt = amt
-    def display(self):
-        print(f"Laptop name: {self.name} and Amount: {self.amt}")
 
-lap1 = LapTop("Victus",75000)
-lap1.display()
+class Student(Person):
+    def __init__(self,name,roll_no):
+        super().__init__(name)
+        self.roll_no = roll_no
 
-
-
-lap2 = LapTop("Asus",85000)
-lap2.display()
-
-lap3 = LapTop("MacBook",125000)
-lap3.display()
-
-lap4 = LapTop("Dell",95000)
-lap4.display()
-
-
-lap5 = LapTop("Acer",65000)
-lap5.display()
-
-
-lap6 = LapTop("Lenovo",70000)
-lap6.display()
-lap7 = LapTop("HP",80000)
-lap7.display()
-
-lap8 = LapTop("MSI",120000)
-lap8.display()
-
-
-lap9 = LapTop("Razer",150000)
-lap9.display()
-
-lap10 = LapTop("Samsung",90000)
-lap10.display()
+std = Student("Sailesh",1001)
+print(f"Name: {std.name}, Roll No: {std.roll_no}")
