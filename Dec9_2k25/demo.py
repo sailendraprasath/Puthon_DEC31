6668f5e01e049268815a30c1f8eedceffa61535f
sailendraprasath <sailendraprasath05@gmail.com>
2025-12-09 07:43:24 +0530

    well

---
+++ Dec9_2k25/demo.py
@@ -0,0 +1,12 @@
+a = 600
+b = 600
+c = 600
+
+if a > b and a > c:
+    print("a is the largest")
+elif b > a and b > c:
+    print("b is the largest")
+elif a == b == c:
+    print("All are equal")
+else:
+    print("c is the largest")   
